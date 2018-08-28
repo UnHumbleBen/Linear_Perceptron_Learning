28fceb76e52a75d0ade7410e3ab6a5017ecc7587
unhumbleben <benjaminsll18@gmail.com>
2018-08-27 23:29:10 -0700

    coin simulation

--- hoeffding_inequality_coins.py
+++ hoeffding_inequality_coins.py
@@ -2,8 +2,10 @@ import random
 
 
 class Coin:
-    def __init__(self):
+    def __init__(self, number_of_flips=None):
         self.coin_log = []
+        if number_of_flips != None:
+            self.flip(number_of_flips)
 
     def flip(self, number_of_flips=None):
         if number_of_flips is None:
@@ -21,11 +23,35 @@ class Coin:
         return number_of_heads / len(self.coin_log)
 
 
+def experiment(number_of_coins, number_of_flips):
+    c_1 = Coin(number_of_flips)
+    c_min = c_1
+    coin_list = [c_1]
+
+    for _ in range(number_of_coins - 1):
+        new_coin = Coin(number_of_flips)
+        coin_list.append(new_coin)
+        if new_coin.frequency_of_head() < c_min.frequency_of_head():
+            c_min = new_coin
+
+    c_rand = coin_list[random.randrange(number_of_coins)]
+
+    return [c_1.frequency_of_head(), c_rand.frequency_of_head(), c_min.frequency_of_head()]
+
+
 number_of_coins = 1000
 number_of_flips = 10
-coin_list = []
-for _ in range(number_of_coins):
-    new_coin = Coin()
-    new_coin.flip(number_of_flips)
-    coin_list.append(new_coin)
-    
+number_of_experiments = 100
+v_1_sum = 0
+v_rand_sum = 0
+v_min_sum = 0
+
+for _ in range(number_of_experiments):
+    result = experiment(number_of_coins, number_of_flips)
+    v_1_sum += result[0]
+    v_rand_sum += result[1]
+    v_rand_sum += result[2]
+
+print("v1:    " + str(v_1_sum / number_of_experiments))
+print("v_rand " + str(v_rand_sum / number_of_experiments))
+print("v_min  %.5f" % (v_min_sum / number_of_experiments))
