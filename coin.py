794365f2f30770cf4e02ce6b4e4526e625be9cc5
unhumbleben <benjaminsll18@gmail.com>
2018-08-28 17:30:16 -0700

    more efficient coin

---
+++ coin.py
@@ -0,0 +1,41 @@
+import numpy as np
+from random import randrange
+
+
+def experiment(number_of_coins, number_of_flips):
+    v_1 = np.count_nonzero(np.random.randint(2, size=number_of_flips)) / number_of_flips
+    random_index = randrange(number_of_coins)
+    v_rand = v_1
+    v_min = v_1
+
+    for i in range(1, random_index):
+        v_new = np.count_nonzero(np.random.randint(2, size=number_of_flips)) / number_of_flips
+        if v_new < v_min:
+            v_min = v_new
+
+    v_rand = np.count_nonzero(np.random.randint(2, size=number_of_flips)) / number_of_flips
+
+    for i in range(random_index + 1, number_of_coins):
+        v_new = np.count_nonzero(np.random.randint(2, size=number_of_flips)) / number_of_flips
+        if v_new < v_min:
+            v_min = v_new
+
+    return [v_1, v_rand, v_min]
+
+
+number_of_coins = 1000
+number_of_flips = 10
+number_of_experiments = 100000
+v_1_sum = 0
+v_rand_sum = 0
+v_min_sum = 0
+
+for _ in range(number_of_experiments):
+    result = experiment(number_of_coins, number_of_flips)
+    v_1_sum += result[0]
+    v_rand_sum += result[1]
+    v_min_sum += result[2]
+
+print("v1:    " + str(v_1_sum / number_of_experiments))
+print("v_rand " + str(v_rand_sum / number_of_experiments))
+print("v_min  %.5f" % (v_min_sum / number_of_experiments))
