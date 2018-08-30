c477e8e53fec96a696fb4f5c6e06c69fa896d137
unhumbleben <benjaminsll18@gmail.com>
2018-08-29 18:44:33 -0700

    added  number_of_out_of_sample_points  as  experiment  function parameter added  function  for finding misclassified points

--- linear_regression.py
+++ linear_regression.py
@@ -5,7 +5,7 @@ from numpy import linalg
 from numpy import matmul
 
 
-def experiment(number_of_training_points):
+def experiment(number_of_training_points, number_of_out_of_sample_points):
     def sign(n):
         if n == 0:
             return 0
@@ -31,6 +31,16 @@ def experiment(number_of_training_points):
 
     def g_function(inputs):
         return sign(dot_product(g_function_weight, inputs))
+
+    def g_misclassified_points():
+        misclassified_points = []
+        for n in range(len(data_set_x)):
+            input = data_set_x[n]
+            actual_y = target_function(input)
+            learned_y = g_function(input)
+            if (actual_y != learned_y):
+                misclassified_points.append(n)
+        return misclassified_points
 
     # choosing two random points in [-1, 1] x [-1, 1]
     first_rand_point_x1 = uniform(-1, 1)
@@ -71,16 +81,11 @@ def experiment(number_of_training_points):
         g_function_weight.append(weight[0])
 
     # calculating fraction of in-sample points
-    number_incorrect_in_sample = 0
-    for input in data_set_x:
-        f_value = target_function(input)
-        g_value = g_function(input)
-        if f_value != g_value:
-            number_incorrect_in_sample += 1
-    fraction_in_sample = number_incorrect_in_sample / number_of_training_points
+    # create list of misclassified points for PLA
+    misclassified_points = g_misclassified_points()
+    fraction_in_sample = len(misclassified_points) / number_of_training_points
 
     # calculating fraction of out-of-sample points
-    number_of_out_of_sample_points = 1000
     number_of_incorrect_out_of_sample = 0
     for _ in range(number_of_out_of_sample_points):
         random_point_x1 = uniform(-1, 1)
@@ -94,15 +99,6 @@ def experiment(number_of_training_points):
 
     # iterating PLA
     number_of_iterations = 0
-
-    misclassified_points = []
-    for n in range(len(data_set_x)):
-        input = data_set_x[n]
-        actual_y = target_function(input)
-        learned_y = g_function(input)
-        if (actual_y != learned_y):
-            misclassified_points.append(n)
-
     while len(misclassified_points) > 0:
         number_of_iterations += 1
 
@@ -116,21 +112,17 @@ def experiment(number_of_training_points):
             g_function_weight[d] = g_function_weight[d] + random_misclassified_point_y * random_misclassified_point_x[d]
 
         # creating new set of misclassified points
-        misclassified_points = []
-        for n in range(len(data_set_x)):
-            input = data_set_x[n]
-            actual_y = target_function(input)
-            learned_y = g_function(input)
-            if (actual_y != learned_y):
-                misclassified_points.append(n)
+        misclassified_points = g_misclassified_points()
 
     return [fraction_in_sample, fraction_out_of_sample, number_of_iterations]
 
+
 number_of_training_points = 10
-number_of_experiments = 2000
+number_of_experiments = 1000
+number_of_out_of_sample_points = 1000
 results = [0, 0, 0]
 for _ in range(number_of_experiments):
-    experiment_results = experiment(number_of_training_points)
+    experiment_results = experiment(number_of_training_points, number_of_out_of_sample_points)
     results[0] += experiment_results[0]
     results[1] += experiment_results[1]
     results[2] += experiment_results[2]
@@ -138,10 +130,7 @@ for _ in range(number_of_experiments):
 avg_fraction_in = results[0] / number_of_experiments
 avg_fraction_out = results[1] / number_of_experiments
 avg_iterations = results[2] / number_of_experiments
-print([avg_fraction_in, avg_fraction_out, avg_iterations])
 
-# testing
-# print("output y: " + str(data_set_y))
-# print("matrix form y: " + str(y_matrix))
-# print(data_set_x)
-# print(x_matrix)
+print("in sample errors:             " + str(avg_fraction_in))
+print("out of sample errors:         " + str(avg_fraction_out))
+print("average number of iterations: " + str(avg_iterations))
