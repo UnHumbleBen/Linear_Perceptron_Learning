3d73a837b29439ccdb1ab410560ed74cae120d0d
unhumbleben <benjaminsll18@gmail.com>
2018-09-12 22:10:23 -0700

    final commit

--- VC_Dimension.py
+++ VC_Dimension.py
@@ -2,5 +2,26 @@ import math
 
 
 def nCr(n, r):
+    if (r > n):
+        return 0
     f = math.factorial
     return f(n) // f(r) // f(n - r)
+
+
+def string(N, m_H):
+    print("m_H(" + str(N) + ") = " + str(m_H))
+
+
+def experiment(q):
+    N = 1
+    m_H = 2
+    string(N, m_H)
+
+    while m_H == 2 ** N:
+        m_H = 2 * m_H - nCr(N, q)
+        N += 1
+        string(N, m_H)
+
+    print("For q = " + str(q) + ", VC dimension is " + str(N - 1))
+
+experiment(100)
